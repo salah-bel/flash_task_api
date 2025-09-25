aa3c0b7bcf9a382f981309ae179e8894d9c4f850
salah-bel <belasalah@gmail.com>
2025-09-25 09:28:44 +0200

    chore: add express app/server skeleton + test route

--- quicktask-backend/src/index.ts
+++ quicktask-backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Flash - Task API running…');
+import express from 'express';
+import cors from 'cors';
+import testRoutes from './routes/test.routes';
+const app = express();
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use( testRoutes);
+// Routes
+
+export default app;
